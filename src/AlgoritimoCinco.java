fe468235732ee1105ac6fba999af01a6e0bdbfbd
Joao-2830 <jpogomes18@gmail.com>
2026-09-14 11:34:47 -0300

    Exerciocio Dois

---
+++ src/AlgoritimoCinco.java
@@ -0,0 +1,11 @@
+public class AlgoritimoCinco {
+    public static void main(String[] args) {
+        int numero;
+        numero = Integer.parseInt(IO.readln("\n digite um numero: "));
+        int antecessor = numero - 1;
+        int sucessor = numero + 1;
+        IO.println("O antecessor:" + antecessor);
+        IO.println("O sucessor:" + sucessor);
+
+    }
+}
---
+++ src/AlgoritimoDois.java
@@ -0,0 +1,8 @@
+public class AlgoritimoDois {
+        public static void main(String[] args) {
+            IO.println("Quem ousa entrar na Sala do Eco?");
+            String nome = IO.readln();
+            IO.println("Ah..." + nome + ". Eu esperava por você.");
+        }
+
+}
