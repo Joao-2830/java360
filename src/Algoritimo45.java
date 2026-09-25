e268a67207ea9a04216a70eb39a444f728926e44
Joao-2830 <jpogomes18@gmail.com>
2026-09-25 11:49:51 -0300

    salvando meu arquivo

--- src/Algoritimo45.java
+++ src/Algoritimo45.java
@@ -2,13 +2,6 @@ import java.util.List;
 public class Algoritimo45 {
     public void main(){
         //java
-
-
-
-
-
-
-
         List<String> tarefas = List.of("teste mesa", "algoritimo", "oo");
         for (String tarefa:tarefas){
             IO.println(tarefa);
--- src/Algoritimo47.java
+++ src/Algoritimo47.java
@@ -1,13 +1,22 @@
 public class Algoritimo47 {
     public void main(){
-        IO.println("Iniciando Algoritimo47");
-        int umnumero = Integer.parseInt(IO.readln("Digite o número "));
-        IO.println(umnumero);
+        int[] numeros = new int[10];
+        for(int i=0;i<=numeros.length;i++){
+
+        }
+
+        }
+        /*IO.println("Iniciando Algoritimo47");
+        int umNumero = Integer.parseInt(IO.readln("Digite o número "));
+        IO.println(umNumero);
 
         int[] numeros = new int[10];
         int soma =0;
-
+        for(int i=0;i<numeros.length;i++){
+            int numero = Integer.parseInt(IO.readln("Digite o número"));
+            numeros[i] = numero;
+            soma+=soma;
+        }
+        IO.println("média:"+soma/numeros.length);*/
 
     }
-    
-}
